7dcdb38a8e7a20f2167ad34f3745c93fa7cfe430
Allard van Helbergen <allard@vanhelbergen.com>
2012-10-14 13:40:20 +0100

    adding debugbar plugin

--- core/wp-config.php
+++ core/wp-config.php
@@ -6,10 +6,8 @@
  */
 
 
-if (file_exists(dirname(__FILE__) . '/wp-config-dev.php')) {
-  include(dirname(__FILE__) . '/wp-config-dev.php');
-  define('WP_LOCAL_DEV', true);
-} else if (file_exists(dirname(__FILE__) . '/wp-config-prod.php')) {
-  include(dirname(__FILE__) . '/wp-config-prod.php');
-  define('WP_LOCAL_DEV', false);
+if ( file_exists( dirname( __FILE__ ) . '/wp-config-dev.php' )) {
+  include( dirname( __FILE__ ) . '/wp-config-dev.php' );
+} else if ( file_exists( dirname( __FILE__ ) . '/wp-config-prod.php' )) {
+  include( dirname( __FILE__ ) . '/wp-config-prod.php' );
 }
