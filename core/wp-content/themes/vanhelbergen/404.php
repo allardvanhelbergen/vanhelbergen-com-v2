5d26aae785c3a6b51b27567f457e7d0cec15ec03
Allard van Helbergen <allard@vanhelbergen.com>
2012-10-29 23:32:36 +0000

    improving 404

--- core/wp-content/themes/vanhelbergen/404.php
+++ core/wp-content/themes/vanhelbergen/404.php
@@ -16,50 +16,24 @@ get_header(); ?>
         <header class="entry-header">
           <h1 class="entry-title">
             <?php _e( 
-                'This is somewhat embarrassing, isn&rsquo;t it?',
+                'Crap. It broke.',
                 'twentyeleven' ); ?>
           </h1>
         </header>
 
         <div class="entry-content">
           <p>
-            <?php _e( 
-                'It seems we can&rsquo;t find what you&rsquo;re looking for. ' .
-                    'Perhaps searching, or one of the links below, can help.', 
-                'twentyeleven' ); ?>
+            404 - Not Found.
           </p>
-
-          <?php get_search_form(); ?>
-
-          <?php the_widget( 
-              'WP_Widget_Recent_Posts', array( 'number' => 10 ), 
-              array( 'widget_id' => '404' ) ); ?>
-
-          <div class="widget">
-            <h2 class="widgettitle">
-              <?php _e( 'Most Used Categories', 'twentyeleven' ); ?>
-            </h2>
-            <ul>
-              <?php wp_list_categories( array(
-                'orderby' => 'count', 'order' => 'DESC',
-                'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-            </ul>
-          </div>
-
-          <?php
-          /* translators: %1$s: smilie */
-          $archive_content = '<p>' . sprintf( 
-                  __( 'Try looking in the monthly archives. %1$s',
-                      'twentyeleven' ),
-                  convert_smilies( ':)' ) ) .
-              '</p>';
-          the_widget( 
-              'WP_Widget_Archives', array('count' => 0 , 'dropdown' => 1 ),
-              array( 'after_title' => '</h2>'.$archive_content ) );
-          ?>
-
-          <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
+          <p>
+            It seems I messed up and wrote some bad code.
+          </p>
+          <p>
+            While my army of minions runs to remedy the situation, why not go 
+            look at another brilliant page on this website through the menu 
+            above. 
+          </p>
+          <p></p>
         </div><!-- .entry-content -->
       </article><!-- #post-0 -->
 
